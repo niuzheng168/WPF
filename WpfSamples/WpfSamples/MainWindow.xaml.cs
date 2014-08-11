0163af2ffab9176fee0af549d33702ac5e81f9cf
niuzheng168 <niuzheng168@gmail.com>
2014-08-11 16:22:00 +0800

    Initial project

---
+++ WpfSamples/WpfSamples/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace WpfSamples
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
