726d9ed6810796d9d3608483f2a0d03dd6facb2c
niuzheng168 <niuzheng168@gmail.com>
2014-08-12 21:35:39 +0800

    Refine code.

--- WpfSamples/WpfSamples/MainWindowViewModel.cs
+++ WpfSamples/WpfSamples/MainWindowViewModel.cs
@@ -2,23 +2,30 @@
 {
     using WpfCommon.ModelBase;
 
-    using WpfSamples.Controls.UserControls;
     using WpfSamples.Controls.ViewModels;
     using WpfSamples.DummyData;
 
     /// <summary>
-    /// The main window view model.
+    ///     The main window view model.
     /// </summary>
     public class MainWindowViewModel : ViewModelBase
     {
-        private EnumRadioButtonGroupViewModel<DebugDummyData.DummyEnum> dummy=new EnumRadioButtonGroupViewModel<DebugDummyData.DummyEnum>();
+        /// <summary>
+        /// The dummy.
+        /// </summary>
+        private EnumRadioButtonGroupViewModel<DebugDummyData.DummyEnum> dummy =
+            new EnumRadioButtonGroupViewModel<DebugDummyData.DummyEnum>();
 
+        /// <summary>
+        /// Gets or sets the dummy. 
+        /// </summary>
         public EnumRadioButtonGroupViewModel<DebugDummyData.DummyEnum> Dummy
         {
             get
             {
                 return this.dummy;
             }
+
             set
             {
                 this.dummy = value;
